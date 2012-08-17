555eaf2e5699c900963b17f8e148e223d7438057
unknown <0xack13@gmail.com>
2012-08-17 17:07:15 +0300

    Adding proper quotes to the full path to avoid whitespace issues

--- virtualbox-change-disk-uuid.bat
+++ virtualbox-change-disk-uuid.bat
@@ -1,9 +1,9 @@
 @echo off
 cls
-cd C:\Program Files\Oracle\VirtualBox
+cd "C:\Program Files\Oracle\VirtualBox"
 echo Simply add the full path of your virtual disk as a parameter to this batch file in the Windows command prompt:
 echo 
-echo EXAMPLE: vb-ch-uuid.bat “C:\Virtual Hard Disks\Exchange-2010.vhd”
+echo EXAMPLE: vb-ch-uuid.bat "C:\Virtual Hard Disks\Exchange-2010.vhd"
 echo 
 echo 
 pause
